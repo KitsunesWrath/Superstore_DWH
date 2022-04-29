57f09051471902740253b69b641b1426bf070323
Monika <58700629+KitsunesWrath@users.noreply.github.com>
2022-04-29 13:30:15 +0200

    procedures execution order

---
+++ 100_execute_procedures.sql
@@ -0,0 +1,28 @@
+/* Execute procedures */
+
+/* LANDING TO STAGING */
+EXEC [Cfg].[LS_Superstore]
+EXEC [Cfg].[LS_People]
+EXEC [Cfg].[LS_Returns]
+
+/* INDEPENDENT DIMS */
+EXEC [Cfg].[LR_Category]
+EXEC [Cfg].[LR_City]
+EXEC [Cfg].[LR_Country]
+EXEC [Cfg].[LR_Country]
+EXEC [Cfg].[LR_Customer]
+EXEC [Cfg].[LR_Order]
+EXEC [Cfg].[LR_People]
+EXEC [Cfg].[LR_Product]
+EXEC [Cfg].[LR_Segment]
+EXEC [Cfg].[LR_Segment]
+EXEC [Cfg].[LR_Ship]
+EXEC [Cfg].[LR_State]
+EXEC [Cfg].[LR_State]
+EXEC [Cfg].[LR_Subcategory]
+
+/* DEPENDENT DIMS */
+EXEC [Cfg].[LR_Region]
+
+/* FACTS */
+EXEC [Cfg].[LF_Superstore]
